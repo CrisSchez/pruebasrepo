57f13415f47961890093e7722a22ef9bca8fcb8f
Cristina Sánchez <cristina.sanchez@cloudera.com>
2022-02-25 13:47:32 +0000

    subir tabla de female

--- Untitled.py
+++ Untitled.py
@@ -29,3 +29,12 @@ spark.sql("select * from default.telco_churn").show()
 df = spark.sql("SELECT * FROM default.telco_churn").toPandas()
 
 dfFemale=df[df['gender']=='Female']
+telcoFemale=spark.sql("select * from default.telco_churn where gender eq 'Female'")
+if ('telco_churn_female' not in list(spark.sql("show tables in default").toPandas()['tableName'])):
+    print("creating the telco_churn_female database")
+    telcoFemale\
+        .write.format("parquet")\
+        .mode("overwrite")\
+        .saveAsTable(
+            'default.telco_churn_female'
+        )
