7e455cd17b41738e82d9957dd4aa7a67c63c4689
Workshop Admin <admin@cloudera.com>
2022-07-20 13:30:39 +0000

    date and time

--- Untitled.py
+++ Untitled.py
@@ -40,4 +40,4 @@ if ('telco_churn_female' not in list(spark.sql("show tables in default").toPanda
         )
         
 #añade comentarios
-# introduce date and time: 18 sep 21 19:00 
+# introduce date and time: 20 07 22 15:30 
